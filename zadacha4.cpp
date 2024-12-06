cd27127f33ad48e4895effe2e53abff1325a2082
shihow dmitri <shihowdima1337@gmail.com>
2024-12-06 11:43:15 +0800

    Add files via upload

---
+++ zadacha4.cpp
@@ -0,0 +1,38 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{  
+	int arr[10];
+	
+	cout << "Enter the array\n";
+	for (int i = 0; i < sizeof(arr)/4; i++) {
+		cin >> arr[i];
+	}
+	
+	
+	
+	int length = 0;
+	int count =  0;
+	for (int i = 0; i < sizeof(arr)/4; i++) {
+		if (arr[i] == 0 ) {
+			int j = i;
+			while(arr[j] == 0) {
+				count++;
+				j++;
+			}
+
+		}
+		if (count > length) {
+			length = count;
+		}
+		count = 0;
+	}
+	cout << "ANSWER:" << length << endl;
+	
+	
+
+}
+
+
---
+++ zadacha8.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x[6];
+	int y[6];
+	cout << "Enter array X:\n";
+	for (int i = 0; i < sizeof(x) / 4; i++) {
+		cin >> x[i];
+	}
+	cout << "Enter array Y:\n";
+	for (int i = 0; i < sizeof(x) / 4; i++) {
+		cin >> y[i];
+	}
+	int k;
+	cout << "Enter the K: \n";
+	cin >> k;
+	bool answer = false;
+	for (int i = 0; i < sizeof(x) / 4; i++) {
+		for (int j = 0; j < k; j++) {
+			if (x[i + j] == y[j]) {
+				answer = true;
+			}
+			else {
+				answer = false;
+				break;
+			}
+		}
+		if (answer) {
+			cout << "YES";
+			break;
+		}
+	}
+	if (!answer) cout << "NO";
+}
+
+
